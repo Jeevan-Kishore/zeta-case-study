90fdfa1689ea1b5b03e683ab872d66503802e3a1
jeevan-kishore <kishor080@gmail.com>
2020-05-16 23:01:48 +0530

    chore(build): update build

---
+++ build/precache-manifest.25843d5b3cbcb9b7387e9d3a49a98b9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78756b80ba1d4923bafdc4fae05b2828",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab733becccbc3b29df44",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "a5f3082bfe4a92374b84",
+    "url": "/static/js/2.09f4e59c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.09f4e59c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab733becccbc3b29df44",
+    "url": "/static/js/main.9e111325.chunk.js"
+  },
+  {
+    "revision": "59314c7a23ccf407ec3e",
+    "url": "/static/js/runtime-main.7d588fce.js"
+  }
+]);
